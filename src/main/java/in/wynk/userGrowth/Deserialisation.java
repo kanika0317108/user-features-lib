0192f2fa13ca270d7cef4db0fbe5b193ad8de45a
Kanika <kanika@wynk.in>
2025-01-14 18:15:36 +0530

    base setup

--- src/main/java/in/wynk/userGrowth/Deserialisation.java
+++ src/main/java/in/wynk/userGrowth/Deserialisation.java
@@ -1,9 +1,7 @@
 package in.wynk.userGrowth;
 
 import in.wynk.CompactedUserFeatures;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class Deserialisation {
     public static CompactedUserFeatures deserialiseToProto(byte[] bytes) throws Exception {
         CompactedUserFeatures message = CompactedUserFeatures.parseFrom(bytes);
--- src/main/java/in/wynk/userGrowth/Serialisation.java
+++ src/main/java/in/wynk/userGrowth/Serialisation.java
@@ -2,9 +2,7 @@ package in.wynk.userGrowth;
 
 import com.google.protobuf.util.JsonFormat;
 import in.wynk.CompactedUserFeatures;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class Serialisation {
     public static byte[] serialize(String json) throws Exception {
         CompactedUserFeatures.Builder structBuilder = CompactedUserFeatures.newBuilder();
