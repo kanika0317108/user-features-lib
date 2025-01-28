a72541ca04fbbdc0f36a58ddeb9c6f318cfc6e0b
Kanika <kanika@wynk.in>
2025-01-28 15:28:54 +0530

    class name change-1

--- src/main/java/in/wynk/userGrowth/UserFeaturesDeserializer.java
+++ src/main/java/in/wynk/userGrowth/UserFeaturesDeserializer.java
@@ -2,7 +2,7 @@ package in.wynk.userGrowth;
 
 import in.wynk.CompactedUserFeatures;
 
-public class UserFeaturesDeserializeSchema {
+public class UserFeaturesDeserializer {
     public static CompactedUserFeatures deserialiseToProto(byte[] bytes) throws Exception {
         return CompactedUserFeatures.parseFrom(bytes);
     }
--- src/main/java/in/wynk/userGrowth/UserFeaturesSerializer.java
+++ src/main/java/in/wynk/userGrowth/UserFeaturesSerializer.java
@@ -2,7 +2,7 @@ package in.wynk.userGrowth;
 import com.google.protobuf.util.JsonFormat;
 import in.wynk.CompactedUserFeatures;
 
-public class UserFeaturesSerializeSchema {
+public class UserFeaturesSerializer {
     public static byte[] serialize(String json) throws Exception {
         CompactedUserFeatures.Builder structBuilder = CompactedUserFeatures.newBuilder();
         JsonFormat.parser().ignoringUnknownFields().merge(json, structBuilder);
