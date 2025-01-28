4f9676e4258c4bb5c81b14c51707ea373b257e1b
Kanika <kanika@wynk.in>
2025-01-28 15:17:31 +0530

    class name change

--- src/main/java/in/wynk/userGrowth/UserFeaturesDeserializeSchema.java
+++ src/main/java/in/wynk/userGrowth/UserFeaturesDeserializeSchema.java
@@ -2,7 +2,7 @@ package in.wynk.userGrowth;
 
 import in.wynk.CompactedUserFeatures;
 
-public class Deserialisation {
+public class UserFeaturesDeserializeSchema {
     public static CompactedUserFeatures deserialiseToProto(byte[] bytes) throws Exception {
         return CompactedUserFeatures.parseFrom(bytes);
     }
